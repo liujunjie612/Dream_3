848016f9149e462e5a82d9db0d7b7909c6cc297c
junjie <1225325960@qq.com>
2017-06-14 00:21:04 +0800

    增加战斗动画（初级）

--- Assets/Scripts/Components/AnimationGif.cs
+++ Assets/Scripts/Components/AnimationGif.cs
@@ -1,48 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System;
 
-namespace Yule.Components
+
+[RequireComponent(typeof(Image))]
+public class AnimationGif : MonoBehaviour
 {
-    [RequireComponent(typeof (Image))]
-    public class AnimationGif : MonoBehaviour
+    public Sprite[] animationSprites;
+    public float animationSpeed = 0.1f;
+    public bool loop = true;
+
+    public Action animationEndCallback;
+
+    private Image _img;
+    private float _curretnTime;
+    private int _index;
+    private float _length;
+    private bool _continue;
+
+    void Awake()
     {
-        public Sprite[] animationSprites;
-        public float animationSpeed = 0.1f;
-        public bool loop = true;
+        _continue = true;
+        _img = GetComponent<Image>();
+        _length = animationSprites.Length;
+    }
 
-        private Image _img;
-        private float _curretnTime;
-        private int _index;
-        private float _length;
-        private bool _continue;
+    private void OnEnable()
+    {
+        _continue = true;
+        _curretnTime = Time.time;
+        _index = 0;
+    }
 
-        void Start()
+    public void SetAnimation(bool play)
+    {
+       this.gameObject.SetActive(true);
+        _img.sprite = animationSprites[0];
+        _continue = play;
+    }
+
+    private void Update()
+    {
+        if (_continue && Time.time - _curretnTime > animationSpeed)
         {
-            _continue = true;
-            _img = GetComponent<Image>();
-            _length = animationSprites.Length;
-        }
-
-        private void OnEnable()
-        {
-            _continue = true;
             _curretnTime = Time.time;
-            _index = 0;
-        }
-
-        private void Update()
-        {
-            if (_continue && Time.time - _curretnTime > animationSpeed)
+            _img.sprite = animationSprites[_index];
+            _img.SetNativeSize();
+            _index++;
+            if (_index >= _length)
             {
-                _curretnTime = Time.time;
-                _img.sprite = animationSprites[_index];
-                _index++;
-                if (_index >= _length)
+                _index = 0;
+                if (!loop)
                 {
-                    _index = 0;
-                    if (!loop)
-                        _continue = false;
+                    _continue = false;
+                    if (animationEndCallback != null)
+                        animationEndCallback();
                 }
             }
         }
---
+++ Assets/Scripts/Main/NearFightManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NearFightManager : MonoBehaviour {
+
+    public AnimationGif wsAttack_1;
+    public AnimationGif wsAttack_2;
+    public AnimationGif wsAttack_3;
+    public AnimationGif wsAttack_4;
+
+    public AnimationGif bossAttack_1;
+    public AnimationGif bossAttack_2;
+    public AnimationGif bossAttack_3;
+    public AnimationGif bossAttack_4;
+    public AnimationGif bossAttack_5;
+    public AnimationGif bossDefeat;
+
+    private AnimationGif _startAnimation;
+    private AnimationGif _endAnimation;
+
+    void Start()
+    {
+        SetFight();
+    }
+
+    public void SetFight()
+    {
+        wsAttack_1.SetAnimation(true);
+        bossDefeat.SetAnimation(false);
+        wsAttack_1.animationEndCallback = () => { bossDefeat.SetAnimation(true); };
+    }
+}
